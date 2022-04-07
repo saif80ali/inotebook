9bab2d6322df38b93445e8ef173b0dd63eac7b12
saif80ali <mdsaif80ali@gmail.com>
2022-04-07 12:33:58 +0530

    my next commit

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const URI = "mongodb://localhost:27017/inotebook"
 
 const connectToMogo = ()=>{
     mongoose.connect(URI,()=>{
-        console.log("MongoDB Connection Successful!")
+        console.log("MongoDB Connection Successful! yaaay")
     })
 }
 
