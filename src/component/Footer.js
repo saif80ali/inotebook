fa3f6e29007d258bbf5f1468738c43092e14565b
saif80ali <mdsaif80ali@gmail.com>
2024-06-29 16:05:56 +0530

    Footer position correction

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-white py-4">
+    <footer className="bg-dark text-white py-4 mt-4">
     <div className="container text-center">
-      <p>&copy; 2024 iNotebook. All rights reserved.</p>
+      <p className='m-0'>&copy; 2024 iNotebook. All rights reserved.</p>
     </div>
   </footer>
   )
