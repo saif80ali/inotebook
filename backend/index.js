47b69b3e517bc49754ba0ce57bdd8b843c3cbd38
saif80ali <mdsaif80ali@gmail.com>
2024-06-30 16:12:02 +0530

    Removing cors config

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,7 @@ require('dotenv').config();
 connectToMogo()
 const app = express()
 const port =  process.env.PORT;
-const frontend_url = process.env.FRONTEND_URL;
-app.use(cors({
-  origin: frontend_url, // Allow requests from this origin
-  methods: 'GET,POST,PUT,DELETE', // Allow these methods
-  credentials: true // Include credentials like cookies in requests
-}))
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth',require('./routes/auth'))
