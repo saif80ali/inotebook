8cb66d8965db7eb96325c086f73ca61605db205f
saif80ali <mdsaif80ali@gmail.com>
2024-07-06 14:44:04 +0530

    Adding come checks

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 connectToMogo()
 const app = express()
 const port =  process.env.PORT || "5000";
+const JWT_SECRET = process.env.JWT_SECRET;
+const URI = process.env.MONGODB_URI
 const frontendurl = process.env.FRONTEND_URL || "http://localhost:3000";
 app.use(cors())
 app.use(express.json())
@@ -14,7 +16,11 @@ app.use(express.urlencoded({ extended: true }));;
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from Express!' });
+  res.json({ message: 'Hello from Express!',
+    port: port,
+    key: JWT_SECRET,
+    uri:URI
+   });
 });
 
 app.listen(port, () => {
