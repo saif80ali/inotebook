b6e3f895e62fb6995d55cefdfa9fbaad56cbdd3a
saif80ali <mdsaif80ali@gmail.com>
2024-07-06 14:40:05 +0530

    Addinng urll encoding

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ const port =  process.env.PORT || "5000";
 const frontendurl = process.env.FRONTEND_URL || "http://localhost:3000";
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));;
 
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
