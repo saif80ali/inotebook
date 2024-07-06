b05643b9faa4d21d3af5d2d2f502f61456171c2b
saif80ali <mdsaif80ali@gmail.com>
2024-07-06 14:04:19 +0530

    changinf folder structure of inotebook

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const URI = process.env.MONGODB_URI;
+const URI = process.env.MONGODB_URI || "mongodb://localhost:27017";
+console.log(URI)
 const connectToMogo = ()=>{
     mongoose.connect(URI,{
         useNewUrlParser: true,
--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,12 @@ require('dotenv').config();
 
 connectToMogo()
 const app = express()
-const port =  process.env.PORT;
+const port =  process.env.PORT || "5000";
+const frontendurl = process.env.FRONTEND_URL || "http://localhost:3000";
 app.use(cors())
 app.use(express.json())
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../backend/build')));
 
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
