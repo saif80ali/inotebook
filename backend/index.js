a5d78a123d40c31f65b1fb3ab9e9a2bb4cb4e4af
saif80ali <mdsaif80ali@gmail.com>
2024-07-06 14:30:13 +0530

    get api changes

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,11 @@ const frontendurl = process.env.FRONTEND_URL || "http://localhost:3000";
 app.use(cors())
 app.use(express.json())
 
-// Serve static files from the React app
-
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from Express!' });
+});
 
 app.listen(port, () => {
   console.log(`iNotebook backend listening on port ${port}`)
